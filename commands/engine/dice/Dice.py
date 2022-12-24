fed01061f295967c15fc7183506cfe78f43afedb
Ex-Opera <88405792+Ex-Opera@users.noreply.github.com>
2022-12-24 10:09:50 +0530

    Used regex to make it simpler

--- commands/engine/dice/Dice.py
+++ commands/engine/dice/Dice.py
@@ -1,4 +1,4 @@
-import random
+import random, re
 
 class Dice:
   def __init__(self, dice_string: str):
@@ -9,10 +9,17 @@ class Dice:
     self.parse_dice_string()
 
   def parse_dice_string(self):
-    # Split dice string into rolls and bonus
-    if '+' in self.dice_string or '-' in self.dice_string or '*' in self.dice_string or '/' in self.dice_string:
-      self.bonus = int(self.dice_string.split('d')[1])
-      self.dice_string = self.dice_string.split('d')[0]
+    # Find all occurrences of +, -, *, or / in dice string
+    bonus_ops = re.finditer(r'[+\-*/]', self.dice_string)
+    try:
+      # Split dice string on first occurrence of bonus op
+      first_bonus_op = next(bonus_ops).start()
+      self.bonus = self.dice_string[first_bonus_op:]
+      self.dice_string = self.dice_string[:first_bonus_op]
+    except StopIteration:
+      # No bonus op found, set bonus to None
+      self.bonus = None
+
     self.rolls = [int(x) for x in self.dice_string.split('d')]
     if len(self.rolls) == 1:
       self.rolls.append(self.rolls[0])
@@ -23,10 +30,8 @@ class Dice:
     rolls = [random.randint(1, self.rolls[1]) for _ in range(self.rolls[0])]
     self.total = sum(rolls)
     if self.bonus:
-      bonus_op = self.dice_string.split('d')[1][0]
-      self.total = {'+': self.total + self.bonus, '-': self.total - self.bonus, 
-                    '*': self.total * self.bonus, '/': self.total / self.bonus}[bonus_op]
+      self.total = eval(f'{self.total}{self.bonus}')
     return self.total
 
-dice = Dice('2d20+5')
+dice = Dice("6d12+5")
 print(dice.roll())
