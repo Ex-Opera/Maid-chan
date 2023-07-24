ee5aea665082d6d688dc2398b7ba128803d61de0
Ex-Opera <requize23@proton.me>
2023-07-24 22:34:32 +0530

    improved readability added descriptive decorators

--- commands/utilities.py
+++ commands/utilities.py
@@ -6,26 +6,25 @@ from discord.ext.commands import bot
 from random import choice
 # from main import Maidchan
 
-
 class Utilities(commands.Cog):
   def __init__(self, Maidchan):
     self.Maidchan = Maidchan
 
-  @commands.command()
+  @commands.command(name="ping", brief="Master i'll show my latency!", help="Master, this command shows how fast I can respond to your commands")
+  async def ping(self, ctx):
+    await ctx.send(f'Pong! {round(self.Maidchan.latency * 1000)}ms')
+
+  @commands.command(name="say", brief="Master i'll repeat what you say", help="Master, this command repeats what you say and deletes your message")
   async def say(self, ctx, *, said):
     await ctx.send(said)
     await ctx.message.delete()
 
-  @commands.command()
+  @commands.command(name="avatar", brief="Master i'll show  the avatar of a user", help="Master, this command shows the avatar of a user. If you don't specify a user, I will show your avatar")
   async def avatar(self, ctx, *, user: discord.Member = None):
     user = user or ctx.author
     await ctx.send(user.avatar.url)
 
-  @commands.command()
-  async def ping(self, ctx):
-    await ctx.send(f'Pong! {round(self.Maidchan.latency * 1000)}ms')
-
-  @commands.command()
+  @commands.command(name="translate", brief="Master i'll translate text to a certain language", help="Master, this command translates text to a language of your choice. You need to specify the language code and the text you want me to translate")
   async def translate(self, ctx, *, input_text):
     language = input_text[:2]
     url = f"https://translation.googleapis.com/language/translate/v2?key={config.translate_key}"
