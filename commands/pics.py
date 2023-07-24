ff528d0dfc61efba2f4044dbf53bedaabdfddf91
Ex-Opera <requize23@proton.me>
2023-07-24 21:03:04 +0530

    replaced requests with aiohttp, removed redundant code

--- commands/pics.py
+++ commands/pics.py
@@ -1,45 +1,36 @@
 import discord
-import requests
+import aiohttp
 import random
 
 class Pics(commands.Cog):
   def __init__(self, Maidchan):
     self.Maidchan = Maidchan
-    self.session = requests.Session()
+    self.session = aiohttp.ClientSession()
 
-  async def send_pic(self, ctx, url):
+  async def send__pic(self, ctx, url, id):
     try:
-      data = self.session.get(url).json()
-      await ctx.channel.send(data['url'])
+      async with self.session.get(url) as response:
+        data = await response.json()
+        await ctx.channel.send(data[id])
     except Exception as err:
       await ctx.channel.send(f'Master, an error occurred: {err}')
 
-  async def get_category(self, ctx, sfw=True):
-    if sfw:
-      categories = ['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 'hug', 'awoo', 'kiss', 'lick', 'pat', 'smug', 'bonk', 'yeet', 'blush', 'smile', 'wave', 'highfive', 'handhold', 'nom', 'bite', 'glomp', 'slap', 'kill', 'kick', 'happy', 'wink', 'poke', 'dance', 'cringe']
-    else:
-      categories = ['waifu', 'neko', 'trap', 'blowjob']
-    if ctx.channel.is_nsfw() == True:
-      return random.choice(categories)
-    else:
-      await ctx.channel.send('M-master, wrong channel!')
-      return None
-
-  @commands.command()
+  @commands.command(name="anime", brief="Sends a random anime picture", help="Sends a random anime picture from a given category or a random one. The available categories are: waifu, neko, shinobu, megumin, bully, cuddle, cry, hug, awoo, kiss, lick, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, kick, happy, wink, poke, dance, cringe.")
   async def anime(self, ctx, *, category=None):
-    category = category or await self.get_category(ctx)
-    if category is not None:
-      await self.send_pic(ctx, f'https://api.waifu.pics/sfw/{category}')
+    category = category or random.choice(['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 'hug', 'awoo', 'kiss', 'lick', 'pat', 'smug', 'bonk', 'yeet', 'blush', 'smile', 'wave', 'highfive', 'handhold', 'nom', 'bite', 'glomp', 'slap', 'kill', 'kick', 'happy', 'wink', 'poke', 'dance', 'cringe'])
+    await self.send_pic(ctx, f'https://api.waifu.pics/sfw/{category}', "url")
 
   @commands.command()
   async def hentai(self, ctx, *, category=None):
-    category = category or await self.get_category(ctx, sfw=False)
-    if category is not None:
-      await self.send_pic(ctx, f'https://api.waifu.pics/nsfw/{category}')
-    
+    if ctx.channel.is_nsfw() == True:
+       category = category or random.choice(['waifu', 'neko', 'trap', 'blowjob'])
+       await self.send_pic(ctx, f"https://api.waifu.pics/nsfw/{category}", "url")
+     else:
+       await ctx.channel.send("M-master, wrong channel!")
+
   @commands.command()
   async def fox(self, ctx):
-    await self.send_pic(ctx, "https://randomfox.ca/floof/")
+    await self.send_pic(ctx, "https://randomfox.ca/floof/", "image")
 
 async def setup(Maidchan):
   await Maidchan.add_cog(Pics(Maidchan))
