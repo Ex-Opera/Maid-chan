b26ce87e23b8857bd8c74b8b5d1ab56b4d1d2fe2
Ex-Opera <requize23@proton.me>
2023-07-24 22:15:35 +0530

    moved anime search to pics

--- commands/utilities.py
+++ commands/utilities.py
@@ -8,7 +8,7 @@ from random import choice
 class Utilities(commands.Cog):
   def __init__(self, Maidchan):
     self.Maidchan = Maidchan
-    
+
   @commands.command()
   async def say(self, ctx, *, said):
     await ctx.send(said)
@@ -18,44 +18,16 @@ class Utilities(commands.Cog):
   async def math(self, ctx, *, calc):
     if '+' in calc or '-' in calc or '/' in calc or '*' in calc or 'sqrt' in calc or '**':
     await ctx.send(eval(calc))
-    
+
   @commands.command()
   async def avatar(self, ctx, *, user: discord.Member=None):
     user = user if user else ctx.author
     avatar = user.avatar_url
     await ctx.send(avatar)
-    
-  @commands.command()
-  async def ping(self, ctx): 
-    await ctx.send(f'Pong! {round(self.Maidchan.latency * 1000)}ms')
 
   @commands.command()
-  async def animesearch(self, ctx, *, img):
-    # Search
-    img = requests.get(f'https://api.trace.moe/search?cutBorders&url={img}')
-    img = json.loads(img.text)
-   # Get anime info
-    anilistQuery = '''
-      query ($id: Int) {
-      Media (id: $id, type: ANIME) {
-        id
-        title {
-        romaji
-        english
-        native
-          }
-        }
-      }'''
-    anilistPOST = requests.post("https://graphql.anilist.co/", json={'query': anilistQuery, 'variables': {'id': img['result'][1]['anilist']}})
-    anilistPOST = str(anilistPOST.text)
-    anilistPOST = json.loads(str(anilistPOST))
-    await ctx.send(f"""
-Similarity: {(str(float((img['result'][1]['similarity'])*100)))[:5]}%
-Anilist: https://anilist.co/anime/{img['result'][1]['anilist']}
-Tittle romaji: {anilistPOST['data']['Media']['title']['romaji']}
-Tittle romaji: {anilistPOST['data']['Media']['title']['english']}
-Episode: {img['result'][1]['episode']}
-Time between: {str(datetime.timedelta(seconds=(img['result'][1]['from'])))[:7]} and {str(datetime.timedelta(seconds=(img['result'][1]['to'])))[:7]}""")
+  async def ping(self, ctx):
+    await ctx.send(f'Pong! {round(self.Maidchan.latency * 1000)}ms')
 
   @commands.command()
   async def translate(self, ctx, *, input_text):
